0d6439cd69cc43ee3096d677f6dbfa62f98c709e
Rebecca Romero <rebeccaromero@Rebeccas-MacBook-Air.local>
2020-09-26 13:37:32 -0400

    first working version

--- converters.py
+++ converters.py
@@ -1,4 +1,7 @@
-# output 25mpg value in kpl
-print(25 * 1.609344 * .2641720524)
+# ask the user to input an MPG value
+mpg = input("What is the MPG?")
+# convert the input into a numeric (float) value 
+mpg = float(mpg)
+print(mpg * 1.609344 * .2641720524)
 
 
